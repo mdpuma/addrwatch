11cbe3462e2fc9c3ce69186bedaff611ed002498
root <root@vh3.amplica.net>
2015-12-08 10:43:45 +0200

    added init file for debian 7 confirmed work on debian 7, 8

--- deb/build.sh
+++ deb/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-fpm -t deb -s dir -C ../DESTINATION -n addrwatch -v 0.8 \
-	-d libevent-2.0-5 -d libpcap0.8 -e
+rm *.deb
+fpm -t deb -s dir -C ../install-destination -n addrwatch -v 0.8 -d libevent-2.0-5 -d libpcap0.8 -e
